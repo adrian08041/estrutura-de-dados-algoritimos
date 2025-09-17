22a5c2bb3cc9b49c9d6c9da01d05f8182128c356
Adrian Messias <90083313+adrian08041@users.noreply.github.com>
2025-09-17 01:09:05 +0000

    adicionado metodos addMeio e removeMeio

--- aula-07/Principal.java
+++ aula-07/Principal.java
@@ -1,15 +1,29 @@
 public class Principal {
     public static void main(String[] args) {
         ListaDupla<String> lista = new ListaDupla<String>("Alunos");
-
-        lista.addInicio("Pedro");
-        lista.addInicio("Miguel");
+        
+        lista.addInicio("rafael");
+        lista.addInicio("miguel");
+        lista.addInicio("gabriel");
         lista.addFinal("asdfas");
 
+
+        lista.addMeio("pedro", 2);
+        lista.imprimeLista();
+
+
+
+        lista.removeMeio(1);
+        lista.imprimeLista();
+
+        lista.removeMeio(2);
+
+lista.imprimeLista();   
+
+        /*  
         lista.imprimeLista();
         lista.removeInicio();
         lista.imprimeLista();
-        lista.removeFinal();
-            
+        lista.removeFinal(); */
     }
 }
